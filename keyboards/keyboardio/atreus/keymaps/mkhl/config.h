bfaa231efc683b6749bae43151fbcd32d6a9ec47
Martin Kühl <martin.kuehl@posteo.net>
2022-02-11 15:08:18 +0100

    wip atreus

---
+++ keyboards/keyboardio/atreus/keymaps/mkhl/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/* Make tap and hold keys more permissive */
+#define PERMISSIVE_HOLD
+
+/* Shorter delay before a tap becomes a hold */
+#define TAPPING_TERM 200
+
+/* Shift keys when held */
+#define AUTO_SHIFT_TIMEOUT 150
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_NUMERIC
